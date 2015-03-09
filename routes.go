a6f68086b20acc58bf43fed99c2f2578e5345aad
John Calabrese <jcalabrese@pivotal.io>
2015-03-09 08:19:11 -0400

    [#89130160] setting up routes
    we now have all the pre-defined routes, stubbed.
    
    just need to add some stubbed controllers

--- routes.go
+++ routes.go
@@ -1,14 +1,39 @@
 package pezdispenser
 
 import (
+	"fmt"
+
 	"github.com/go-martini/martini"
 )
 
 const (
+	ApiVersion = "v1"
 	indexRoute = "/"
+	leasePath  = "lease"
+	lockPath   = "lock"
+	typePath   = "type"
+	itemPath   = "item"
+	invGuid    = ":inventoryTypeGuid"
+	typeGuid   = ":inventoryItemGuid"
+)
+
+var (
+	UrlLeaseBase = fmt.Sprintf("/%s/%s", ApiVersion, leasePath)
+	UrlLockBase  = fmt.Sprintf("/%s/%s", ApiVersion, lockPath)
+	UrlTypeGuid  = fmt.Sprintf("/%s/%s", typePath, typeGuid)
+	UrlItemGuid  = fmt.Sprintf("/%s/%s", itemPath, invGuid)
 )
 
 func InitRoutes(m *martini.ClassicMartini) {
-	// do something here below
-	//m.Get(indexRoute, randomController)
+
+	m.Group(UrlLeaseBase, func(r martini.Router) {
+		r.Post(UrlTypeGuid, RandomController)
+		r.Post(UrlItemGuid, RandomController)
+		r.Delete(UrlItemGuid, RandomController)
+	})
+
+	m.Group(UrlLockBase, func(r martini.Router) {
+		r.Post(UrlItemGuid, RandomController)
+		r.Get(UrlItemGuid, RandomController)
+	})
 }
