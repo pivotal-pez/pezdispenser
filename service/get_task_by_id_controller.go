64d7bf3783a2f774e051d1788e06d8b6d8be6d7e
John Calabrese <jcalabrese@pivotal.io>
2015-08-22 18:04:47 -0400

    [#101772930] final touch logging minor refactor
    just a little tidying up and adding of logs
    that was missed in prior commit...

--- service/get_task_by_id_controller.go
+++ service/get_task_by_id_controller.go
@@ -1,6 +1,7 @@
 package pezdispenser
 
 import (
+	"fmt"
 	"log"
 
 	"labix.org/v2/mgo"
@@ -12,29 +13,35 @@ import (
 
 //GetTaskByIDController - this is the controller to handle a get task call
 func GetTaskByIDController(taskServiceURI string, collectionDialer integrations.CollectionDialer) martini.Handler {
+
+	var (
+		err      error
+		dialInfo *mgo.DialInfo
+	)
+	if dialInfo, err = ParseURL(taskServiceURI); err == nil {
+		log.Println("parsed uri successfully: ", taskServiceURI, dialInfo)
+
+	} else {
+		panic(fmt.Sprintf("can not parse given URI %s due to error: %s", taskServiceURI, err.Error()))
+	}
+
 	return func(params martini.Params, log *log.Logger, r render.Render) {
 		var (
 			err        error
-			dialInfo   *mgo.DialInfo
 			response   interface{} = &err
 			statusCode             = FailureStatusResponseTaskByID
 			collection integrations.Collection
+			task       = new(Task)
+			taskID     = params["id"]
 		)
-		defer func() {
-			if collection != nil {
-				collection.Close()
-			}
-		}()
-		task := new(Task)
-		taskID := params["id"]
 
-		if dialInfo, err = ParseURL(taskServiceURI); err == nil {
-
-			if collection, err = collectionDialer(taskServiceURI, dialInfo.Database, TaskCollectionName); err == nil {
-				err = collection.FindOne(taskID, task)
-				statusCode = SuccessStatusResponseTaskByID
-				response = task
-			}
+		if collection, err = collectionDialer(taskServiceURI, dialInfo.Database, TaskCollectionName); err == nil {
+			defer collection.Close()
+			log.Println("collection dialed successfully")
+			err = collection.FindOne(taskID, task)
+			log.Println("task search complete")
+			statusCode = SuccessStatusResponseTaskByID
+			response = task
 		}
 		r.JSON(statusCode, response)
 	}
