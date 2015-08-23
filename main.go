08031d03be96bb89e6cda7fe55e6a318122c20b2
John Calabrese <jcalabrese@pivotal.io>
2015-08-23 12:27:00 -0400

    [#101296514] adding lots of output to main for debug

--- main.go
+++ main.go
@@ -11,16 +11,24 @@ import (
 )
 
 func main() {
+	fmt.Println("here")
 	appEnv, _ := cfenv.Current()
+	fmt.Println("appEnv raw:", appEnv)
 	validatorServiceName := os.Getenv("UPS_PEZVALIDATOR_NAME")
 	targetKeyName := os.Getenv("UPS_PEZVALIDATOR_TARGET")
+	fmt.Println("osGetEnv", validatorServiceName, targetKeyName)
 	service, _ := appEnv.Services.WithName(validatorServiceName)
+	fmt.Println("myservice", service)
 	validationTargetURL := fmt.Sprintf("%s", service.Credentials[targetKeyName])
+	fmt.Println("validationUrl:", validationTargetURL)
 	m := martini.Classic()
 
 	if appEnv, err := cfenv.Current(); err == nil {
+		fmt.Println("setting up middleware keycheck")
 		keyCheckHandler := keycheck.NewAPIKeyCheckMiddleware(validationTargetURL).Handler()
+		fmt.Println("initroutes")
 		pez.InitRoutes(m, keyCheckHandler, appEnv)
+		fmt.Println("routes initialized")
 
 	} else {
 		panic(fmt.Sprint("Experienced an error trying to grab current cf environment:", err.Error()))
