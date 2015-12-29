c955852d7ba51d60e04990e7bdecc09e0612d573
John Calabrese <jcalabrese@pivotal.io>
2015-12-29 11:56:13 -0500

    [#110807020] fixing empty data panic bug
    the dispenser client would panic if it
    could not find a data array with
    elements.
    
    this is now more gracefully handled w/
    proper error returned

--- pdclient/get_requestid_from_task_test.go
+++ pdclient/get_requestid_from_task_test.go
@@ -17,13 +17,50 @@ var _ = Describe("given a GetRequestIDFromTaskResponse func", func() {
 		)
 		BeforeEach(func() {
 			tr := getTaskResponse(controlRequestID)
-			requestID = GetRequestIDFromTaskResponse(tr)
+			requestID, _ = GetRequestIDFromTaskResponse(tr)
 		})
 		It("then it should return properly parse out and return that requestid", func() {
 			Ω(requestID).Should(Equal(controlRequestID))
 		})
 	})
+	Context("when an object not containing a populated data array is returned", func() {
+		It("then it should not panic", func() {
+
+			tr := getNoDataTaskResponse()
+			Ω(func() {
+				GetRequestIDFromTaskResponse(tr)
+			}).ShouldNot(Panic())
+		})
+		Context("when calling getrequestidfromtaskresponse without panic with an empty data oboject", func() {
+
+			var requestID string
+			var err error
+			BeforeEach(func() {
+				tr := getNoDataTaskResponse()
+				requestID, err = GetRequestIDFromTaskResponse(tr)
+			})
+			It("then it should return an error", func() {
+				Ω(requestID).Should(BeEmpty())
+				Ω(err).Should(HaveOccurred())
+			})
+		})
+	})
 })
+
+func getNoDataTaskResponse() (taskResponse TaskResponse) {
+	var taskResponseString = `{
+		"ID": "56748ece7bc989001d000002",
+		"Timestamp": 1450479310174826830,
+		"Expires": 0,
+		"Status": "complete",
+		"Profile": "agent_task_long_running",
+		"CallerName": "m1.small",
+		"MetaData": {}
+	}`
+	json.Unmarshal([]byte(taskResponseString), &taskResponse)
+	return
+
+}
 
 func getTaskResponse(requestID string) (taskResponse TaskResponse) {
 	var taskResponseString = fmt.Sprintf(`{
